127837701c16eae4db046f7fbac407f06a6e0d2b
armandoltx <armando@armandoltx.com>
2016-09-22 18:41:18 +1000

    fixing the problem when numbers are not typed

--- js/main.js
+++ js/main.js
@@ -13,6 +13,11 @@ window.onload=function(){
 
   buttonStart.addEventListener("click",function(){
     var inputMin = document.getElementById("Inputminutes");
+    if ( isNaN (inputMin) ){
+      alert ('Must be a number');
+      inputMin.value = '00';
+      return false;
+    }
     var minutes = inputMin.value;
     // it has to be inside the function otherwise when the page is loaded the value is empty... and it wont take any new value because it is stored in the var before I typed it.
 
